2bb422f8cd0604034c10d9994e79dea3b3837b9f
Fahad Ahmed <fahad.ahmed95@live.com>
2022-09-23 14:27:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email='fahad.ahmed95@live.com',
     description='Communicate with AWS infrastructure',
     packages=[''],
-    python_requires='>=3.7.0',
     url='',
     install_requires=[
         'boto3',
