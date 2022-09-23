5eb2f19755dede89515428328e5c322cb26934ee
Fahad Ahmed <fahad.ahmed95@live.com>
2022-09-23 14:43:34 +0200

    restructuring

--- setup.py
+++ setup.py
@@ -6,15 +6,16 @@ setup(
     author='Fahad Ahmed',
     author_email='fahad.ahmed95@live.com',
     description='Communicate with AWS infrastructure',
-    packages=[''],
-    url='',
+    python_requires='>3.7.0',
+    packages=find_packages(),
+    url='https://github.com/FahadahmedK/awslake',
     install_requires=[
         'boto3',
         'botocore',
         'paramiko'
     ],
     license='Apache License 2.0',
-    classifier = [
+    classifier=[
         'Development Status :: 4 - Beta',
         'Framework :: AWS CDK',
         'Programming Language :: Python :: 3.10'
