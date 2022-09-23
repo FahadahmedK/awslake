8c8ed811c47a9332a03bba34f8876e1ffa2148c7
Fahad Ahmed <fahad.ahmed95@live.com>
2022-09-23 12:54:41 +0200

    adding initial structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awslake',
+    version='1.0',
+    author='Fahad Ahmed',
+    author_email='fahad.ahmed95@live.com',
+    description='Communicate with AWS infrastructure',
+    packages=find_packages(),
+    python_requires='>=3.9.0',
+    url='',
+    install_requires=[
+        'boto3',
+        'json',
+        'logging',
+        'botocore',
+        'paramiko'
+    ],
+    license='Apache License 2.0',
+    classifier = [
+        'Development Status :: 4 - Beta',
+        'Framework :: AWS CDK',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
